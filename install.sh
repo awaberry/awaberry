1d35c9ddc6e4fa2f21348fd98ced6c27a173a528
imagenotion <aw@imagenotion.com>
2025-08-04 10:38:23 +0200

    added install.sh script to orchestrate the overall installation

--- install.sh
+++ install.sh
@@ -10,13 +10,15 @@ if ! command -v brew >/dev/null 2>&1; then
   exit 1
 fi
 
-echo "1) downloading the awaberry.rb script"
+echo "1) downloading the awaberry.rb installation scripts"
 curl -s https://raw.githubusercontent.com/awaberry/awaberry/main/awaberry.rb -o awaberry.rb
+curl -s https://raw.githubusercontent.com/awaberry/awaberry/main/macbrewinstaller.sh -o macbrewinstaller.sh
 
 echo "2) installing the awaberry.rb script"
 brew reinstall --build-from-source ./awaberry.rb
 
 echo "3) installing the awaberry client"
+chmod +x macbrewinstaller.sh
 ./macbrewinstaller.sh
 
 
